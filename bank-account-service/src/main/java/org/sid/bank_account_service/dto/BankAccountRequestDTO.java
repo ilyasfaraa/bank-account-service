6881a172d277d0c6da2891ce2028c983e1accb41
Ilyas Faraa-Ettarh <faraailyas8@gmail.com>
2024-11-11 16:32:53 +0100

    Initial commit

---
+++ bank-account-service/src/main/java/org/sid/bank_account_service/dto/BankAccountRequestDTO.java
@@ -0,0 +1,15 @@
+package org.sid.bank_account_service.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bank_account_service.enums.AccountType;
+
+import java.util.Date;
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class BankAccountRequestDTO {
+    private Double balance ;
+    private String currency;
+    private AccountType type;
+
+}
